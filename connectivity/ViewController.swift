bd0bb8d541cda7d3bb4afee9aa2f478e2c7ab53a
Ivan Štajcer <istajcer@etfos.hr>
2022-02-22 09:49:23 +0100

    Initial Commit

---
+++ connectivity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  connectivity
+//
+//  Created by Ivan Stajcer on 22.02.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
